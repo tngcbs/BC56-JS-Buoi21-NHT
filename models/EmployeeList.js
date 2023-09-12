3c2c35e8164f75f90a059e2fa27073bb8dc056c5
tgncbs <tung.cbs23@gmail.com>
2023-09-12 21:27:04 +0700

    Update models

--- models/EmployeeList.js
+++ models/EmployeeList.js
@@ -4,4 +4,24 @@ function EmployeeList() {
     this.addEmployee = function (employee) {
         this.employees.push(employee);
     };
+
+    this.searchEmployee = function (username) {
+        var employeeIndex = -1;
+
+        for (var index = 0; index < this.employees.length; index++) {
+            var currentEmployee = this.employees[index];
+            if (currentEmployee.username === username) {
+                employeeIndex = index;
+                break;
+            }
+        }
+        return employeeIndex;
+    }
+
+    this.deleteEmployee = function (username) {
+        var employeeIndex = this.searchEmployee(username);
+        if (employeeIndex != -1) {
+            this.employees.splice(employeeIndex, 1);
+        }
+    }
 }
