6201a79995ec2ade42460ad987d82fe3dea8d1ae
tgncbs <tung.cbs23@gmail.com>
2023-09-12 20:29:08 +0700

    Update object

--- controllers/main.js
+++ controllers/main.js
@@ -2,3 +2,22 @@ function getElement(selector) {
     return document.querySelector(selector);
 }
 
+function getInputValue() {
+    var username = getElement("#tknv").value;
+    var fullname = getElement("#name").value;
+    var email = getElement("#email").value;
+    var password = getElement("#password").value;
+    var startDate = getElement("#datepicker").value;
+    var baseSalary = +getElement("#luongCB").value;
+    var position = getElement("#chucvu").value;
+    var workingHours = +getElement("#gioLam").value;
+
+    var employee = new Employee(username, fullname, email, password, startDate, baseSalary, position, workingHours);
+    return employee;
+}
+
+getElement("#btnThemNV").onclick = getInputValue;
+
+function addEmployee() {
+
+}
---
+++ models/EmployeeList.js
@@ -0,0 +1,3 @@
+function EmployeeList() {
+    this.employees = [];
+}
